268c72b7f449c6b14a4a3e8decf76250d94244db
Pip85 <78318926+Pip85@users.noreply.github.com>
2021-06-21 08:31:36 -0400

    Add bar chart init function

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,79 @@
 //======================================================================
 
 // Create bar chart of sample results
-d3.json("data/samples.json").then ((sample) => {
-    var sampleData = sample;
-    console.log(sampleData);
-})
+
+function init() {
+    d3.json("data/samples.json").then ((sample) => {
+        var sampleData = sample;
+        console.log(sampleData);
+        var initialSample = sampleData.samples[0];
+        console.log(initialSample); 
+
+        var trace1 = {
+        x: initialSample.sample_values,
+        y: initialSample.otu_ids,
+        type: "bar",
+        orientation: "h"
+        };
+        var data = [trace1];
+    
+        Plotly.newPlot("bar", data);        
+    });      
+    alert("Watch out!");
+}
+
+
+
+function bar() {
+    d3.json("data/samples.json").then ((samples) => {
+        var sampleSlice = samples.slice(0, 10);
+        console.log(sampleSlice);         
+
+        var trace1 = {
+            x: sampleSlice.map(row => row.sample_values),
+            y: sampleSlice.map(row => row.otu_ids),
+            type: "bar",
+            orientation: "h"
+            };
+        var data = [trace1];
+    
+        Plotly.newPlot("bar", data);        
+    }); 
+}
+
+init();
+
+d3.select("#selDatset").on("change",selDataset);
+
+function optionChange(choiceValue) {
+    // var choice = d3.select("#selDataset");
+    // var choiceValue = choice.value;
+    alert(choiceValue);
+
+}
+
+
+        
+        
+//        
+
+//         var data = [trace1];
+    
+//         Plotly.newPlot("bar", data);
+//     }); 
+// }
+// d3.json("data/samples.json").then ((sample) => {
+//     var sampleData = sample;
+//     console.log(sampleData);
+//     var sampleData = sampleData.samples.slice(0, 10);
+//     console.log(sampleData);
+//     var trace1 = {
+//         x: sampleData.map(row => row.sample_values),
+//         y: sampleData.map(row => row.otu_ids),
+//         type: "bar"
+//         orientation: "h"
+//     };
+//     var data = [trace1];
+
+//     Plotly.newPlot("bar", data);
+// });
