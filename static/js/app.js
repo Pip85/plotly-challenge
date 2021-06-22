aa842ced77ef5fe1183ac72a14975ab5240ea4a4
Pip85 <78318926+Pip85@users.noreply.github.com>
2021-06-22 16:47:34 -0400

    Code for initial bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -2,118 +2,9 @@
 // Create dashboard for Belly Button Biodiversity
 //======================================================================
 
-// Create Bar Chart
+// Create initial bar and bubble charts
 //----------------------------------------------------------------------
 
-// Create initial bar chart for first sample
-
-// function init() {
-//     d3.json("data/samples.json").then ((firstSample) => {
-//         var initialSample = firstSample.samples[0];
-//         console.log(initialSample); 
-        
-//         // var sortSample = Object.entries(initialSample).forEach(([key, value]) => {
-//         //     console.log(key, value);
-//         // })
-
-//         // var slicedSample = sampleID.map(String);
-//         // console.log(slicedSample);
-
-//         // prepY = []
-//         // getY = slicedSample.forEach(id => {
-//         //     prepY.push("OTU "${id});
-//         //     console.log(prepY);
-//         // });
-//         initialSampleValues = initialSample.sample_values.slice(0, 10)
-//         inititalSampleIds = initialSample.otu_ids.slice(0,10);
-        
-//         var trace1 = {
-//         x: initialSampleValues,
-//         y: inititalSampleIds,
-//         type: "bar",
-//         orientation: "h",
-//         text: initialSample.otu_labels.slice(0, 10),
-//         marker: {width: .5}
-//         };
-
-//         var data = [trace1];
-
-//         var layout = {
-//             title:  "Top Ten Bacteria Cultures Found"
-                
-//         //     // yaxis: {
-//         //     //     conve: "strict",
-//         //     //     dtick:  initialSample.otu_ids.slice(0, 10),
-//         //     //     tickvals: initialSample.otu_ids.slice(0, 10),
-                
-//         //     // }
-//         }
-    
-//         Plotly.newPlot("bar", data, layout);        
-//     });
-// }
-
-//----------------------------------------------------------------------------------------------------------------------
-
-// function bar() {
-//     d3.json("data/samples.json").then ((samples) => {
-//         var sampleSlice = samples.slice(0, 10);
-//         console.log(sampleSlice);         
-
-//         var trace1 = {
-//             x: sampleSlice.map(row => row.sample_values),
-//             y: sampleSlice.map(row => row.otu_ids),
-//             type: "bar",
-//             orientation: "h"
-//             };
-//         var data = [trace1];
-    
-//         Plotly.newPlot("bar", data);        
-//     }); 
-// }
-
-// init();
-
-// d3.select("#selDatset").on("change",selDataset);
-
-// function optionChange(choiceValue) {
-//     // var choice = d3.select("#selDataset");
-//     // var choiceValue = choice.value;
-//     alert(choiceValue);
-
-// }
-
-
-        
-        
-//        
-
-//         var data = [trace1];
-    
-//         Plotly.newPlot("bar", data);
-//     }); 
-// }
-// d3.json("data/samples.json").then ((sample) => {
-//     var sampleData = sample;
-//     console.log(sampleData);
-//     var sampleData = sampleData.samples.slice(0, 10);
-//     console.log(sampleData);
-//     var trace1 = {
-//         x: sampleData.map(row => row.sample_values),
-//         y: sampleData.map(row => row.otu_ids),
-//         type: "bar"
-//         orientation: "h"
-//     };
-//     var data = [trace1];
-
-//     Plotly.newPlot("bar", data);
-// init();
-
-//================================================================
-// Bubble Chart
-//================================================================
-
-// Create function to show plots with first sample on page open
 function init() {
     
     // Pull data from samples.json for use in initial plots
@@ -149,77 +40,36 @@ function init() {
             orientation: "h",
             text: labelSlice
         }
-
         var data = [trace1];
-
         var layout = {
             title:  "Top Ten Bacteria Cultures Found"
         }
-
         Plotly.newPlot("bar", data, layout);
 
         // Create initial Bubble Chart
-        var allIds = [];
-        var allValues = [];
-        var allLabels = [];
-
-        samples.forEach((item) => {
-            Object.entries(item).forEach(([key, value]) => {
-                if (key === "otu_ids") {
-                    if (!(value in allIds)) {
-                        allIds.push(value)
-                    };
-                };    
-            });
-        })        
-        //         }
-        //         else if (key === "sample_values") {
-        //             allValues.push(value);
-        //         }
-        //         else {
-        //             allLabels.push(value);
-        //         }
-        //     });              
-        // })
-
-        // console.log(allIds);
-        // console.log(allValues);
-        // console.log(allLabels) 
-
-        var allIds = samples.map(item => {
-            return item.otu_ids;
-        });
-        console.log(allIds);
-
-        // samples.forEach((keys) => {
-        //     console.log(keys);
-        //     Object.entries(keys).forEach(([key,value]) => {
-        //         console.log(key, value);
-        //         var allIds = keys.otu_ids;
-        //         var allValues = keys.sample_values;
-        //         var allLabels = keys.otu_labels;
-        //         console.log(allIds);
-        //         console.log(allValues);
-        //         console.log(allLabels)
-
-        //         var trace2 = {
-        //             x: allIds,
-        //             y: allValues,
-        //             mode: "markers",
-        //             marker: {
-        //                 size: allIds
-        //             }        
-        //         }
-        
-        //         var bubbleData = [trace2];
-        
-        //         var layout = {
-        //             title:  "Bacteria Cultures per Sample"
-        //         }
-        
-        //         Plotly.newPlot("bubble", bubbleData, layout); 
-        //     });
-        // })
-    });           
+       var allValues = initialSample.sample_values;
+       var allIds = initialSample.otu_ids;
+       var allLabels = initialSample.otu_labels;
+       console.log(allValues);
+       console.log(allIds);
+       console.log(allLabels);
+       
+        var trace2 = {
+            x: allIds,
+            y: allValues,
+            text: allLabels,
+            mode: "markers",
+            marker: {
+                size: allValues,
+                color: allIds,
+                colorscale: "Earth"
+            }
+        };
+        var bubbleData = [trace2];
+        var bubbleLayout = {
+            title:  "Bacteria Cultures Per Sample"
+        }
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+    });    
 }
 init();
